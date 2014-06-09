4878dcabb9fc8df346ea92890567f16a8e61e53e
kylekhanhn <kylekhanhn@users.noreply.github.com>
2014-06-08 21:36:05 -0300

    added texture for scythe item

--- forge-1.7.2/src/main/java/thirdworld/ItemScythe.java
+++ forge-1.7.2/src/main/java/thirdworld/ItemScythe.java
@@ -2,12 +2,17 @@ package thirdworld;
 
 import net.minecraft.item.Item;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 
 public class ItemScythe extends Item {
+	
 	//Constructor
 	public ItemScythe() {
-		setMaxStackSize(1);
-		setCreativeTab(CreativeTabs.tabMisc);
+		setMaxStackSize(1); //Allow only 1 scythe per inventory slot
+		setCreativeTab(CreativeTabs.tabTools); //Put the scythe in the tools tab
 		setUnlocalizedName("itemScythe");
+		setTextureName("thirdworld:itemScythe"); //Set the texture
 	}
+	
 }
